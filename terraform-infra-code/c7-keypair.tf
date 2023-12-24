49213142f29bc7c626131fbc1a887287457bd48f
Mohammed Aslaan <81412984+Mohammedaslaan@users.noreply.github.com>
2023-12-24 21:20:29 +0530

    adding versioning in ami name

--- terraform-infra-code/c7-keypair.tf
+++ terraform-infra-code/c7-keypair.tf
@@ -7,7 +7,10 @@ resource "aws_key_pair" "kp" {
   key_name   = "myKey"       # Create a "myKey" to AWS!!
   public_key = tls_private_key.pk.public_key_openssh
 
-  provisioner "local-exec" { # Create a "myKey.pem" to your computer!!
-    command = "[ -e mykey.pem ] && rm -f mykey.pem && echo '${tls_private_key.pk.private_key_pem}' > myKey.pem && chmod 400 myKey.pem"
-  }
+ provisioner "local-exec" {
+  command = <<EOT
+    [ -e mykey.pem ] && rm -f mykey.pem && echo "${tls_private_key.pk.private_key_pem}" > myKey.pem && chmod 400 myKey.pem
+  EOT
+}
+
 }
