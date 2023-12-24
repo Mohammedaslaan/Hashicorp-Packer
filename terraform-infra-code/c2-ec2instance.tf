68bf9bc56023f65c136c2b660647bc87342e422c
Mohammed Aslaan <81412984+Mohammedaslaan@users.noreply.github.com>
2023-12-24 15:58:19 +0530

    adding terraform code to create infra

---
+++ terraform-infra-code/c2-ec2instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "myec2vm" {
+  ami = data.aws_ami.new-created-ami.id
+  instance_type = var.instance_type_list[0]
+  #user_data = filebase64("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "Ec2 Demo ${count.index}"
+  }
+  count = 1
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [
+    aws_security_group.terraform2-SG-ssh.id,
+    aws_security_group.terraform2-SG-web.id
+  ]
+}
---
+++ terraform-infra-code/c5-ami-datasource.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "new-created-ami" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["packer_ubuntu_tomcat9"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
